27f13c5b8dca6c54c53cde1002b1d814d29794b4
Excel Dwi Oktavianto <excelokta@gmail.com>
2016-09-19 18:05:07 +0700

    Create cekip.php

---
+++ cekip.php
@@ -0,0 +1,31 @@
+<?php
+function getLink($url){
+   $curl_handle=curl_init();
+   curl_setopt($curl_handle,CURLOPT_URL,$url);
+   curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,0);
+   curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
+   $result = curl_exec($curl_handle);
+   curl_close($curl_handle);
+  
+   return $result;
+}
+$file_contents = getLink("http://cekip.com");
+preg_match('/<h2>Hostname: (.*?)<\/h2>/s',$file_contents,$ip);
+preg_match('/<h2>City: (.*?)<\/h2>/s',$file_contents,$kota);
+preg_match('/<h2>Country: (.*?)<\/h2>/s',$file_contents,$negara);
+
+$city = "";
+if($kota[1] == "") {
+  $city = "Tidak Ditemukan";
+} else {
+  $city = $kota[1];
+}
+
+echo "\n==================================\n";
+echo "[-] IP Address: ".$ip[1]."\n";
+echo "[-] Kota: ".$city."\n";
+echo "[-] Negara: ".$negara[1]."\n";
+echo "==================================\n";
+echo "=   AUTHOR: N07HY | Version 0.5  =\n";
+echo "==================================\n\n";
+?>
